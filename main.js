591d1a5d101712ef156f1349d609902343b54a1b
hamza-sadeq <hamzasadeq41@gmail.com>
2023-01-08 13:07:26 +0200

    portfolio

---
+++ main.js
@@ -0,0 +1,13 @@
+function openNav(){
+  document.getElementById("aside").style.width = "200px" ;
+  document.getElementById("close-btn").style.display = "block" ;
+  document.getElementById("close-btn").style.position = "absolute" ;
+  document.getElementById("close-btn").style.top = "4.5rem" ;
+  document.getElementById("close-btn").style.right = "14rem" ;
+  document.getElementById("close-btn").style.transition = "0.3s ease" ;
+}
+
+function closeNav(){
+  document.getElementById("aside").style.width = "0" ; 
+  document.getElementById("close-btn").style.display = "none" ;
+}`  `
